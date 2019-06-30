d1f0faa6bd7e1cc5a2d111228e4e349ffa4f93eb
accidentalrebel <accidentalrebel@gmail.com>
2019-06-30 18:52:54 +0800

    Added function that inserts image.

--- arebel-blog-helper.el
+++ arebel-blog-helper.el
@@ -7,7 +7,7 @@
 	 (tags (nth 5 (org-heading-components)))
 	 (content (org-get-entry))
 	 (date (progn (outline-up-heading 1)
-		      (nth 4 (org-heading-components))))	)
+		      (nth 4 (org-heading-components)))))
     (shell-command
      (format "%s %s %s %s %s %s %s"
 	     "arebel-blog-helper"
@@ -18,3 +18,14 @@
 	     (concat "\"" tags "\"")
 	     (concat "\"" content "\"")))
     (find-file (concat "~/blog/karlo.licudine.me/content/" slug ".md"))))
+
+(defun arebel-insert-image ()
+  "Insert the selected file into the blog article.  It automatically copies the file into the correct image folder."
+  (interactive)
+  (let* ((file-path (read-file-name "Get file name:"))
+	 (full-file-name (car (last (split-string file-path "/"))))
+	 (file-name (car (split-string full-file-name "\\."))))
+    (message "fullfilename: %s" full-file-name)
+    (message "filename: %s" file-name)
+    (insert (concat "![" file-name "]({attach}/images/" full-file-name ")"))
+    (copy-file file-path (concat "~/blog/karlo.licudine.me/content/images/" full-file-name))))
