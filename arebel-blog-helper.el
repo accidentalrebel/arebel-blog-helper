f1a6983545e11d5e1883db0a8544f6fddad10868
accidentalrebel <accidentalrebel@gmail.com>
2019-06-30 23:26:35 +0800

    Made sure slug is downcased.

--- arebel-blog-helper.el
+++ arebel-blog-helper.el
@@ -2,7 +2,7 @@
   "Convert a org-journal entry into a Pelican blog entry by parsing the entry and then calling a Python script that does the conversion."
   (interactive)
   (let* ((title (read-string "Enter title: "))
-	 (slug (replace-regexp-in-string " " "-" (replace-regexp-in-string "-" " " title)))
+	 (slug (downcase (replace-regexp-in-string " " "-" (replace-regexp-in-string "-" " " title))))
 	 (time (nth 4 (org-heading-components)))
 	 (tags (nth 5 (org-heading-components)))
 	 (content (org-get-entry))
